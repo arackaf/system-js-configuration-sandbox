acbda43c9c25aec397c482507d3fa25586e5f3ed
Adam Rackis <adamrackis@gmail.com>
2016-05-25 18:40:30 -0500

    woo hoo - resolution

--- runTests.js
+++ runTests.js
@@ -1,17 +1,17 @@
 var config = {
     packages: {
         //but it works with /v0
-        '/v0/': {
+        '': {
             defaultExtension: 'js',
-            map: {
-                text: 'assets/text'
-            },
             meta: {
                 'vendor/util1.global.js': { format: 'global' }
             }
         }
     },
-    baseURL: '/v0/'
+    baseURL: '/v0/',
+    map: {
+        text: 'assets/text'
+    },
 };
 
 window.require = System.amdRequire;
@@ -21,22 +21,22 @@ window.define = System.amdDefine;
 System.config(config);
 
 //to reproduce this error, change `'/v0/'` to just `'/'`
-System.import('vendor/util1.global.js').then(() => console.log('loaded directly'));
+//System.import('vendor/util1.global.js').then(() => console.log('loaded directly'));
 //ALSO FAILS - tasks loads vendor/util1.global
-//System.import('tasks/tasks').then(() => console.log('TASKS LOADED'));
+System.import('tasks/tasks').then(() => console.log('TASKS LOADED'));
 
 //------------------------------------------------------
 
 //Problems with getting the text loader
 
 //This fails - it does not map text.js correctly
-//Promise.all([
-//    System.import('contacts/contacts'),
-//    System.import('contacts/contacts.htm!text')
-//]).then(([obj]) => checkit(() => obj.name == 'contacts', 'contacts loaded'));
+Promise.all([
+    System.import('contacts/contacts'),
+    System.import('contacts/contacts.htm!text')
+]).then(([obj]) => checkit(() => obj.name == 'contacts', 'contacts loaded'));
 
 //This works - contactsWithHtml correctly pulls down text.js from it's specified location
-System.import('contacts/contactsWithHtml').then(obj => checkit(() => obj.name == 'contacts', 'contacts loaded'));
+//System.import('contacts/contactsWithHtml').then(obj => checkit(() => obj.name == 'contacts', 'contacts loaded'));
 
 
 function checkit(expr, message){
